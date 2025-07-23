ae0ab2473c44f4b17ce53c6dae2871175975ce09
33BI <ingus.buneris@gmail.com>
2025-07-23 15:39:01 +0300

    Commit

--- frontend/tests/snapshots.spec.js
+++ frontend/tests/snapshots.spec.js
@@ -1,6 +1,6 @@
-const { test, expect } = require('@playwright/test');
+// const { test, expect } = require('@playwright/test');
 
-test('Homepage should match visual snapshot', async ({ page }) => {
-  await page.goto('http://localhost:3000');
-  expect(await page.screenshot()).toMatchSnapshot('homepage.png', { threshold: 0.2 });
-});
+// test('Homepage should match visual snapshot', async ({ page }) => {
+//   await page.goto('http://localhost:3000');
+//   expect(await page.screenshot()).toMatchSnapshot('homepage.png', { threshold: 0.2 });
+// });
